00b4315b2fa5a7f219231f302be3eb02e9c7f243
Sam Ruwe <ruwesam@gmail.com>
2024-06-05 08:22:12 -0400

    Update

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Nixos config flake";
+  description = "Sam's NixOS Config";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -10,17 +10,17 @@
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
         ./hosts/sam/configuration.nix
-        inputs.home-manager.nixosModules.default
-	./modulebundle.nix
-	{
-	  home-manager.useUserPackages = true;
-	  home-manager.users.sam = import ./hosts/sam/home.nix;
-	}
+	    ./modulebundle.nix
+        home-manager.nixosModules.home-manager
+        {
+	      home-manager.useUserPackages = true;
+	      home-manager.users.sam = import ./hosts/sam/home.nix;
+	    }
       ];
     };
   };
--- hosts/sam/home.nix
+++ hosts/sam/home.nix
@@ -1,19 +1,16 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+    ./cli.nix
+  ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "sam";
-  home.homeDirectory = "/home/sam";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+  home = {
+    username = "sam";
+    homeDirectory = "/home/sam";
+    stateVersion = "24.05";
+  };
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -36,21 +33,6 @@
     # '')
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
-
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
   # shell provided by Home Manager. If you don't want to manage your shell
@@ -67,21 +49,25 @@
   #
   #  /etc/profiles/per-user/sam/etc/profile.d/hm-session-vars.sh
   #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
+
   home.enableNixpkgsReleaseCheck = false;
 
   # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+  # programs.home-manager.enable = true;
 
   programs.zsh = {
       enable = true;
+      zplug = {
+        enable = true;
+        plugins = [
+          { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
+          { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
+        ];
+      };
       shellAliases = {
           ll = "ls -l";
-	  update = "sudo nixos-rebuild switch --flake '.#default'";
-	  cl = "clear";
-	  aaaa = "echo hello!";
+	      update = "sudo nixos-rebuild switch --flake '.#default'";
+          cl = "clear";
       };
   };
 }
