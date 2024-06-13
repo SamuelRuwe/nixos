3a6439db89d1340b703318cc529632d79530c83b
Sam Ruwe <ruwesam@gmail.com>
2024-06-13 08:09:43 -0400

    feat: Configure zsh

--- home/zsh.nix
+++ home/zsh.nix
@@ -3,15 +3,22 @@
 {
   programs.zsh = {
       enable = true;
+      syntaxHighlighting.enable = true;
       dotDir = ".config/zsh";
+
       oh-my-zsh = {
           enable = true;
           theme = "agnoster";
       };
+
       shellAliases = {
           ll = "ls -l";
 	      update = "sudo nixos-rebuild switch --flake '.#default'";
           cl = "clear";
       };
+  };
+  programs.thefuck = {
+      enable = true;
+      enableZshIntegration = true;
   };
 }
