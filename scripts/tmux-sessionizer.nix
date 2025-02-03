0dfc714237e3564532dc201934d31875def61d2e
SamuelRuwe <ruwesam@gmail.com>
2025-02-03 06:15:08 -0500

    feat: Add custom hydration to tmux-sessionizer

--- scripts/tmux-sessionizer.nix
+++ scripts/tmux-sessionizer.nix
@@ -1,6 +1,26 @@
 { pkgs }:
 
 pkgs.writeShellScriptBin "tmux-sessionizer" ''
+  switch_to() {
+      if [[ -z $TMUX ]]; then
+          tmux attach-session -t $1
+      else
+          tmux switch-client -t $1
+      fi
+  }
+
+  has_session() {
+      tmux list-sessions | grep -q "^$1:"
+  }
+
+  hydrate() {
+      if [ -f $2/.tmux-sessionizer ]; then
+          tmux send-keys -t $1 "source $2/.tmux-sessionizer" c-M
+      elif [ -f $HOME/.tmux-sessionizer ]; then
+          tmux send-keys -t $1 "source $HOME/.tmux-sessionizer" c-M
+      fi
+  }
+
   if [[ $# -eq 1 ]]; then
       selected=$1
   else
@@ -15,19 +35,14 @@ pkgs.writeShellScriptBin "tmux-sessionizer" ''
   tmux_running=$(pgrep tmux)
 
   if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-      tmux new-session -s $selected_name -c $selected
-      exit 0
+      tmux new-session -ds $selected_name -c $selected -n editor
+      hydrate $selected_name $selected
   fi
 
-  if ! tmux has-session -t=$selected_name 2> /dev/null; then
-      tmux new-session -ds $selected_name -n editor -c $selected
-      tmux new-window -d -n server -c $selected -t $selected_name
-      tmux send-keys -t $selected_name:editor 'vi' Enter
+  if ! has_session $selected_name; then
+      tmux new-session -ds $selected_name -c $selected -n editor
+      hydrate $selected_name $selected
   fi
 
-  if [[ -z $TMUX ]]; then
-      tmux attach-session -t $selected_name
-  else
-      tmux switch-client -t $selected_name
-  fi
+  switch_to $selected_name
 ''
